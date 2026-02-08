16663435d64302dc7d54cc3efeddc9020c177683
jamaludeen750@gmail.com <jamal@MyPC.>
2026-02-08 06:45:57 +0000

    mongodb setup

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
 
-app.listen(3000, (req, res) => {
-    console.log("Server running on Port 3000")
-});
 
 app.get("/", (req, res) => {
 
     res.send("Hello from Express API");
 })
+
+mongoose.connect(MONGO_URI)
+    .then(() => {
+        app.listen(3000, (req, res) => {
+            console.log(`Server running on Port ${PORT}`)
+        });
+        console.log("DB connected")
+    })
+    .catch(() => {
+        console.log("Error to connect to DB")
+    })
